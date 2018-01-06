c0957b233010a6951459f19a90421835b303a4ed
m00k <christian.bican@gmail.com>
2018-01-06 07:49:13 +0100

    feat(main): add styles

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 
 require('ace-css/css/ace.css');
 require('font-awesome/css/font-awesome.css');
+require('./scss/main.scss');
 
 // Require index.html so it gets copied to dist
 require('./index.html');
